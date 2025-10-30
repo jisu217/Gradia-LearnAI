7388af6801e4f85f34c6e5d11c831fdbde8f46f3
jisu217 <164330581+jisu217@users.noreply.github.com>
2025-10-30 11:16:46 +0900

    Add files via upload

---
+++ gradia-learnai-backend/src/test/java/gradia_learnai_backend/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package gradia_learnai_backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
